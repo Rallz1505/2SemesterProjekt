97a496294aaefa59018b7334cfcecdc178ad60f3
Test <rasmuschristensen1505@gmail.com>
2026-05-06 10:40:37 +0200

    Fuck git

--- SallWhiskyDestillery/src/GUI/ForsidePane.java
+++ SallWhiskyDestillery/src/GUI/ForsidePane.java
@@ -4,11 +4,11 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
-import java.awt.*;
 
 public class ForsidePane extends Application {
 
@@ -22,44 +22,33 @@ public class ForsidePane extends Application {
         stage.show();
     }
 
+    private Label lblAdmin, lblDestillatør;
+    private Button btnRegistrerFad, btnFadoversigt;
+
+
+
     public void initContent(GridPane pane){
         pane.setHgap(20);
         pane.setVgap(20);
         pane.setPadding(new Insets(20));
         pane.setAlignment(Pos.CENTER);
 
-        Button btnAdmin = new Button("Admin");
-        btnAdmin.setPrefWidth(150);
 
-        Button btnDest = new Button("Destillatør");
-        btnDest.setPrefWidth(150);
+        // Admin
+        lblAdmin = new Label("Admin");
+        pane.add(lblAdmin, 0,0);
+        btnRegistrerFad = new Button("Registrer fad");
+        pane.add(btnRegistrerFad,0,1);
+        btnFadoversigt = new Button("Fadoversigt");
+        pane.add(btnFadoversigt,0,2);
 
-        pane.add(btnAdmin, 0, 0);
-        pane.add(btnDest, 1, 0);
 
-        btnAdmin.setOnAction(e -> openAdminWindow());
-        btnDest.setOnAction(e -> openDestWindow());
+
+
+
     }
 
-    private void openAdminWindow() {
-        Stage stage = new Stage();
-        stage.setTitle("Admin panel");
-        AdminPane pane = new AdminPane();
-        Scene scene = new Scene(pane, 600, 400);
 
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private void openDestWindow() {
-        Stage stage = new Stage();
-        stage.setTitle("Destillatør panel");
-        DestillatørPane pane = new DestillatørPane();
-        Scene scene = new Scene(pane, 600, 400);
-
-        stage.setScene(scene);
-        stage.show();
-    }
 
 
 }
--- SallWhiskyDestillery/src/GUI/RegistrerFadPane.java
+++ SallWhiskyDestillery/src/GUI/RegistrerFadPane.java
@@ -1,8 +1,10 @@
 package GUI;
 
 import Controller.Controller;
+import Model.Leverandør;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -10,7 +12,10 @@ import javafx.stage.Stage;
 
 public class RegistrerFadPane extends GridPane {
 
-    private TextField txfId, txfStorrelse, txfLeverandor, txfTidligereIndhold, txfNuvMaengde;
+    private TextField txfId, txfStorrelse, txfTidligereIndhold, txfNuvMaengde;
+
+    private ComboBox<Leverandør> cbhLeverandør;
+    private Stage stage;
 
     public RegistrerFadPane(){
         initStorage();
@@ -34,8 +39,10 @@ public class RegistrerFadPane extends GridPane {
 
         Label lblLeverandor = new Label("Leverandør:");
         this.add(lblLeverandor,0,2);
-        txfLeverandor = new TextField();
-        this.add(txfLeverandor,1,2);
+        cbhLeverandør = new ComboBox<>();
+        this.add(cbhLeverandør,1,2);
+
+        cbhLeverandør.getItems().addAll(Controller.getLeverandører());
 
         Label lblTidligereIndhold = new Label("Tidligere indhold:");
         this.add(lblTidligereIndhold,0,3);
@@ -66,11 +73,11 @@ public class RegistrerFadPane extends GridPane {
     public void gemFad(){
         int id = Integer.parseInt(txfId.getText().trim());
         int storrelse = Integer.parseInt(txfStorrelse.getText().trim());
-        String leverandor = txfLeverandor.getText().trim();
+        Leverandør leverandor = cbhLeverandør.getValue();
         String tidligereIndhold = txfTidligereIndhold.getText().trim();
         int nuvMaengde = Integer.parseInt(txfNuvMaengde.getText().trim());
 
-        Controller.createFad(storrelse,id,leverandor,tidligereIndhold,nuvMaengde);
+        Controller.createFad(storrelse,id,leverandor,tidligereIndhold,nuvMaengde, null);
 
         Stage stage = (Stage) this.getScene().getWindow();
         stage.close();
